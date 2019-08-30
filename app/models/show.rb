c87a0f99ba0102885579dfeeb808990d50654c89
Tom White <eugene.white@gmail.com>
2019-08-30 14:59:46 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   end
   
   def self.most_popular_show
-    Show.where("rating = ?", self.highest_rating)
+    Show.where("rating = ?", self.highest_rating)[0]
   end
   
   def self.lowest_rating
